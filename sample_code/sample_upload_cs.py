10ed8288b4dcc73b277e37d466aa3670f9e1564b
Koya Okuse <88079640+happykoya@users.noreply.github.com>
2025-06-15 15:07:28 +0900

    ADD:FirestoreとCloud Storageの連携確認

---
+++ sample_code/sample_upload_cs.py
@@ -0,0 +1,38 @@
+from google.oauth2 import service_account
+from google.cloud import storage, firestore
+
+# サービスアカウントキーの読み込み
+credentials = service_account.Credentials.from_service_account_file(
+    "credentials/ogp-owner.json",
+    scopes=["https://www.googleapis.com/auth/cloud-platform"]
+)
+
+# 明示的にプロジェクトIDを指定（必要なら）
+project_id = credentials.project_id  # ← 実際のGCPプロジェクトIDに置き換えてください
+#print(project_id)
+# Cloud Storage クライアント
+storage_client = storage.Client(credentials=credentials, project=project_id)
+bucket = storage_client.bucket("our-growth-bucket")
+
+# Firestore クライアント
+db = firestore.Client(credentials=credentials, project=project_id)
+
+# ドキュメント取得と確認
+doc_ref = db.collection("sample-collection").document("sample-id")
+doc = doc_ref.get()
+
+metadata = doc.to_dict()
+print("✅ file_path:", metadata["file_path"])
+file_path = metadata["file_path"]
+
+blob = bucket.blob(file_path)
+# Cloud Storageへアップロード
+blob.upload_from_filename("sample_code/empty.txt")
+
+# Firestoreにメタデータ登録
+db.collection("sample-collection").document("sample-id").update({
+    "file_path": file_path,
+    "user_id": "student_1212",
+    "uploaded_at": firestore.SERVER_TIMESTAMP,
+    "file_type": "txt"
+})
