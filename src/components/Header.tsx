75a7c6a2d30ccf130b3142c9e5676a234a5d3134
Kenjiro56 <hiroken506@gmail.com>
2025-06-29 20:07:17 +0900

    fix/header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <header className="bg-blue-600 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">GCP Study App</h1>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <Link href="/" className="hover:underline">Home</Link>
-            </li>
-            <li>
-              <Link href="/avator" className="hover:underline">Avator</Link>
-            </li>
-            {/* <li>
-              <Link href="/lesson" className="hover:underline">Lesson</Link>
-            </li> */}
-          </ul>
-        </nav>
+      <div className="container mx-auto flex justify-center items-center">
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src="/serviceLogo.svg"
+            alt="Manabo Logo"
+            width={200}
+            height={80}
+            className="h-10 w-auto"
+            priority
+          />
+        </Link>
       </div>
     </header>
   );
