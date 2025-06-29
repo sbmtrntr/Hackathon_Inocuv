f7ec5adbcb47bbf0b58544afe7aac407ed3a64a6
k-mizoguch1 <kmizoguchi107@gmail.com>
2025-06-29 17:08:11 +0900

    feat:VRMとVRMAファイルの追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,21 +4,21 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <header className="bg-blue-600 text-white p-4">
-      <div className="container mx-auto flex justify-center items-center">
+      <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">GCP Study App</h1>
-        {/* <nav> */}
-          {/* <ul className="flex space-x-4"> */}
-            {/* <li>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
               <Link href="/" className="hover:underline">Home</Link>
-            </li> */}
-            {/* <li>
+            </li>
+            <li>
               <Link href="/avator" className="hover:underline">Avator</Link>
-            </li> */}
+            </li>
             {/* <li>
               <Link href="/lesson" className="hover:underline">Lesson</Link>
             </li> */}
-          {/* </ul> */}
-        {/* </nav> */}
+          </ul>
+        </nav>
       </div>
     </header>
   );
